3d83f294d571ac92f1de9bcc640e2f2e46103656
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2025-02-16 01:58:14 +0100

    Add: Implement changes in the deploy.yml file

--- api/router.py
+++ api/router.py
@@ -1,6 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import Response
 
 from api.routes import books
 
+# Dependency to Add `ngrok-skip-browser-warning` Header
+async def add_ngrok_skip_header(request: Request, response: Response):
+    response.headers["ngrok-skip-browser-warning"] = "true"
+    return response
+
+
+
 api_router = APIRouter()
-api_router.include_router(books.router, prefix="/books", tags=["books"])
+
+api_router.include_router(books.router, prefix="/books", tags=["books"], dependencies=[Depends(add_ngrok_skip_header)])
+
